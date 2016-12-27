d36256794454560c77cfee39d9368b307f9e60fa
AK33M <Akeem@YAHIKO.local>
2016-12-27 08:28:11 +0200

    Dat Binding & Pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 
 @NgModule({
-    imports: [BrowserModule],
+    //Angular and Third Party Modules go in the 'imports' array
+    imports: [
+      BrowserModule,
+      FormsModule
+    ],
+    //The App's Directives, Components and Pipes are declared in the 'declarations' array.
     declarations: [
         AppComponent,
         ProductListComponent
