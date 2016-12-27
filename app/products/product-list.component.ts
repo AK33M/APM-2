85fcf810692a0aab3e36bf5408e7f01907c7e315
AK33M <Akeem@YAHIKO.local>
2016-12-27 11:26:12 +0200

    Services and Dependency Injection

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service'
 
 @Component({
     selector: 'pm-products',
@@ -13,39 +14,22 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: IProduct[] = [
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
+    listFilter: string;
+    products: IProduct[];
 
-    toggleImage(): void{
-      this.showImage = !this.showImage;
+    constructor(private _productService: ProductService) {
+
     }
 
-    ngOnInit(): void{
-      console.log('Initializing ' + this.pageTitle + ' Component');
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+         this.products = this._productService.getProducts()
     }
 
     onRatingClicked(message: string): void {
-      this.pageTitle = 'Product List: ' + message;
+        this.pageTitle = 'Product List: ' + message;
     }
 }
