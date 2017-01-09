02ebd02435ccc2348a2e8577447f0cd12c06724a
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-01-09 14:40:51 +0000

    Angular modules, part 1

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,43 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { ProductDetailGuard } from './products/product-guard.service';
-import { ProductListComponent } from './products/product-list.component';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
     //Angular and Third Party Modules go in the 'imports' array
     imports: [
-      BrowserModule,
-      FormsModule,
-      HttpModule,
-      RouterModule.forRoot([
-        { path: 'products', component: ProductListComponent },
-        { path: 'product/:id', component: ProductDetailComponent,
-                                canActivate: [ ProductDetailGuard] },
-        { path: 'welcome', component: WelcomeComponent },
-        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-        { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
-      ])
+        BrowserModule,
+        HttpModule,
+        RouterModule.forRoot([
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+        ]),
+        ProductModule,
     ],
+
     //The App's Directives, Components and Pipes are declared in the 'declarations' array.
     declarations: [
         AppComponent,
-        ProductListComponent,
-        ProductFilterPipe,
-        StarComponent,
-        ProductDetailComponent,
         WelcomeComponent
     ],
-    providers: [ ProductDetailGuard ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
