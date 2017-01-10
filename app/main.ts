ff1f1eccdc24239b5439f089e1132d4e83b0712d
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-01-10 10:46:07 +0000

    added commented code for Aot

--- app/main.ts
+++ app/main.ts
@@ -3,3 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// //for AoT.. smaller applcations
+// import { platformBrowser } from '@angular/platform-browser';
+// import { AppModuleNgFactory } from './app.module.ngfactory';
+//
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
